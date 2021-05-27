8afa0c42d7f156c8fd300ccc92f2b0a0656ed733
Fabio Costa <fabio.costa@transferoswiss.ch>
2021-05-26 23:07:58 -0300

    Create main contract

--- contracts/RPSToken.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.8.4;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract RPSToken is ERC20 {
-    constructor() public ERC20("RockPaperScissors Token", "RPS"){
-        _mint(msg.sender, 1000000000000000000000000);
-    }
-}
---
+++ contracts/RockPaperScissors.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+
+contract RockPaperScissors is ERC20 {
+
+    using Address for address;
+    using SafeERC20 for IERC20;
+
+
+    IERC20 public token;
+
+    constructor(address _token) 
+        ERC20("RockPaperScissorsToken", "RPS")
+    {
+        token = IERC20(_token);
+    }
+
+    function balance() public view returns (uint256) {
+        return token.balanceOf(address(this));
+    }
+
+    function deposit(uint256 _amount) public {
+        // Amount must be greater than zero
+        require(_amount > 0, "amount cannot be 0");
+
+        // Transfer MyToken to smart contract
+        token.safeTransferFrom(msg.sender, address(this), _amount);
+
+        // Mint RockPaperScissorsToken to msg sender
+        _mint(msg.sender, _amount);
+    }
+
+    function depositAll() public {
+        // Deposit all of RockPaperScissorsToken from msg sender
+        deposit(token.balanceOf(msg.sender));
+    }
+
+    function withdraw(uint256 _amount) public {
+        // Burn RockPaperScissorsToken from msg sender
+        _burn(msg.sender, _amount);
+
+        // Transfer MyTokens from this smart contract to msg sender
+        token.safeTransfer(msg.sender, _amount);
+    }
+}
